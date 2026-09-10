fb31a84043cf6ee43f24aec35c17b8013ecea53d
Eso-Net-Live <maxcraftadvandgam@gmail.com>
2026-09-10 07:17:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE="mapapodiyua-pwa-gps-v3";
-const SHELL=["./","./index.html","./manifest.json","./offline.html","./icons/icon-192.png","./icons/icon-512.png","./icons/maskable-512.png"];
+const SHELL=["./","./app.html","./manifest.json","./offline.html","./icons/icon-192.png","./icons/icon-512.png","./icons/maskable-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;let u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request).then(r=>{if(r.ok)caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./offline.html"))))});
